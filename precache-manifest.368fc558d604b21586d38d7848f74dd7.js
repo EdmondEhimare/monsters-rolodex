5097c5d5bf3f8cf5d8c6f650358f2393c0647f94
edmondokosun <angelehimare1@gmail.com>
2019-12-07 17:32:26 +0100

    Updates

---
+++ precache-manifest.368fc558d604b21586d38d7848f74dd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf9debf9230dc987521cf5cc788d622b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "155c2a7f26d4129f9bee",
+    "url": "/monsters-rolodex/static/css/main.b218ff4a.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "155c2a7f26d4129f9bee",
+    "url": "/monsters-rolodex/static/js/main.82096aae.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
