a19f9b4c09be9cecf01a6a96d7fa04fbd157bcea
edmondokosun <angelehimare1@gmail.com>
2019-12-07 16:55:06 +0100

    added the search logic and SeachBox component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { CardList } from "./componets/cardList/CardList";
+import SearchBox from './componets/searchBox/SearchBox.jsx'
 import "./App.css";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ""
     };
   }
 
@@ -16,9 +18,16 @@ class App extends React.Component {
       .then(users => this.setState({ monsters: users }));
   }
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+      <h1> </h1>
+        <SearchBox
+          placeholder = "search monsters"
+          handleChange = {e => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
--- src/componets/cardList/CardList.jsx
+++ src/componets/cardList/CardList.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card } from "../Card/Card";
+import { Card } from "../card/Card";
 import "./CardList.css";
 
 export const CardList = props => {
